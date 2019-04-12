163c3038fd414a506cedee82150c945cc3235334
mavroian <mavflorin@gmail.com>
2019-04-13 01:38:08 +0900

    Work in progress , but passing tests

--- src/Graph.js
+++ src/Graph.js
@@ -1,9 +1,50 @@
 class Graph {
   constructor() {
     this.nodes = {};
+    this.size = 0;
+  }
+  addNode(value) {
+    this.nodes[value] = {};
+    this.size++;
+    return;
+  }
+  addEdge(from, to) {
+    if (!this.nodes[from] || !this.nodes[to]) {
+      return "Invalid node value";
+    }
+    if (Array.isArray(this.nodes[from])) {
+      this.nodes[from].push(to);
+    } else {
+      this.nodes[from] = [to];
+    }
+    if (Array.isArray(this.nodes[to])) {
+      this.nodes[to].push(from);
+    } else {
+      this.nodes[to] = [from];
+    }
+
+    return;
+  }
+  removeNode(value) {
+    let count = 0;
+    delete this.nodes[value];
+    this.size--;
+    for (let keys in this.nodes) {
+      if (this.nodes[keys][value]) {
+        delete this.nodes[keys][keys];
+      }
+    }
+    return;
+  }
+  contains(value) {
+    return this.nodes.hasOwnProperty(value);
+  }
+  removeEdge(from, to) {
+    delete this.nodes[from][to];
+    delete this.nodes[to][from];
   }
 }
-
+module.exports = Graph;
 /*
 |XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 |X                               X
