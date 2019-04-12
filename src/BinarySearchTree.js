a6d2a0686109395ab0018d6464b577f0370efbaf
Go Nakano and Mavroian <g.nakano555+mavflorin@gmail.com>
2019-04-12 18:25:15 +0900

    Added some implementation on BST

--- src/BinarySearchTree.js
+++ src/BinarySearchTree.js
@@ -1,5 +1,30 @@
-class BinarySearchTree {}
-
+class BinarySearchTree {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+  insert(value) {
+    function searchTree(node) {
+      if (node.value > value) {
+        if (!node.left) {
+          node.left = new BinarySearchTree(value);
+        } else {
+          searchTree(node.left);
+        }
+      }
+      if (node.value < value) {
+        if (!node.right) {
+          node.right = new BinarySearchTree(value);
+        } else {
+          searchTree(node.right);
+        }
+      }
+    }
+    searchTree(this);
+  }
+}
+module.exports = BinarySearchTree;
 /*
 |XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 |X                               X
